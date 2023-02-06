c4cab769a0d90c09acdf9da2e9588f1dae433727
Bruno Sanguineti <SESA671943@se.com>
2023-02-06 15:22:27 -0300

    change app module to new course unit

--- angular-course/src/app/app.module.ts
+++ angular-course/src/app/app.module.ts
@@ -1,27 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CockpitComponent } from './cockpit/cockpit.component'
+import { ServerElementComponent } from './server-element/server-element.component'
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipies/recipes.component';
-import { RecipeListComponent } from './recipies/recipies-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipies/recipies-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent
+    ServerElementComponent,
+    CockpitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
