c1a0bd6c83d4ae6265d6fe44bd88469f79941835
Bruno Sanguineti <SESA671943@se.com>
2023-02-06 15:21:41 -0300

    push inputs to array

--- angular-course/src/app/app.component.ts
+++ angular-course/src/app/app.component.ts
@@ -5,6 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'TestServer', content: 'just a test 11'}];  
+ 
+  onServerAdded(serverData: {serverName: string, serverContent: string}){
+    console.log("onServerAdded method");
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    })
+  }
 
-export class AppComponent { 
+  onBluePrintAdded(bluePrintData: {serverName: string, serverContent: string}){
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    })
+  }
 }
